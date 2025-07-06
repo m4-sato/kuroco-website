be9e6637a7d42acd14a9de46664435240390e330
m4-sato <you@example.com>
2025-07-06 14:56:22 +0000

    update

--- my-next-project/app/_libs/microcms.ts
+++ my-next-project/app/_libs/microcms.ts
@@ -1,3 +1,17 @@
+import { createClient } from "microcms-js-sdk";
+import type {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSListContent,
+} from "microcms-js-sdk";
+
+export type Member = {
+  name: string;
+  position: string;
+  profile: string;
+  image: MicroCMSImage;
+} & MicroCMSListContent;
+
 export type Category = {
   name: string;
 };
@@ -10,4 +24,24 @@ export type News = {
   };
   publishedAt: string;
   createdAt: string;
+};
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("Please set MICROCMS_SERVICE_DOMAIN in .env.local");
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("Please set MICROCMS_API_KEY in .env.local");
+}
+
+const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+export const getMembersList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Member>({
+    endpoint: "members",
+    queries,
+  });
+  return listData;
 };
