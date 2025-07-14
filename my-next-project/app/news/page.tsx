86849793e72016a705c7b72dde91d0a30af7fe0e
m4-sato <you@example.com>
2025-07-14 14:57:34 +0000

    7-2session

--- my-next-project/app/news/page.tsx
+++ my-next-project/app/news/page.tsx
@@ -1,5 +1,6 @@
 import { getNewsList } from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
 export default async function Page() {
   const { contents: news } = await getNewsList();
