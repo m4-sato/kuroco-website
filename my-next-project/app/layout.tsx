b0147a1116cc468598e8f0d66826448779d9fa65
m4-sato <you@example.com>
2025-07-20 08:37:25 +0000

    update

--- my-next-project/app/layout.tsx
+++ my-next-project/app/layout.tsx
@@ -1,6 +1,24 @@
 import "./globals.css";
+import type { Metadata } from "next";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("http://localhost:3000"),
+  title: {
+    template: "%s | シンプルなコーポレートサイト",
+    default: "シンプルなコーポレートサイト",
+  },
+  description: "Next.jsで始める",
+  openGraph: {
+    title: "シンプルなコーポレートサイト",
+    description: "Next.jsで始めるモダンCMSサイト",
+    images: ["/ogp.png"],
+  },
+  alternates: {
+    canonical: "http://localhost:3000",
+  },
+};
 
 export default function RootLayout({
   children,
--- my-next-project/app/news/[slug]/page.tsx
+++ my-next-project/app/news/[slug]/page.tsx
@@ -16,6 +16,25 @@ type Props = {
 
 // export const revalidate = 60;
 
+export async function generateMetadata({
+  params,
+  searchParams,
+}: Props): Promise<Metadata> {
+  const data = await getNewsDetail(params.slug, {
+    draftKey: searchParams.dk,
+  });
+
+  return {
+    title: data.title,
+    description: data.description,
+    openGraph: {
+      title: data.title,
+      description: data.description,
+      images: [data?.thumbnail?.url ?? ""],
+    },
+  };
+}
+
 export default async function Page({ params, searchParams }: Props) {
   const data = await getNewsDetail(params.slug, {
     draftKey: searchParams.dk,
